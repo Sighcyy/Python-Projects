29c60b82ab7b14af47a33ae7cdf925aa5ccda5ec
Sighcyy <saisumedhar.a@gmail.com>
2026-06-11 14:39:05 -0500

    Add files via upload

---
+++ Intermediate/MailingLetters Project/main.py
@@ -0,0 +1,15 @@
+PLACEHOLDER = "[name]"
+
+
+
+with open("./Input/Names/invited_names.txt", "r") as file:
+    names = file.readlines()
+
+
+with open("./Input/Letters/starting_letter.txt", "r") as file:
+    letter_content = file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_content.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.docx", "w") as completed_letter:
+             completed_letter.write(new_letter)
