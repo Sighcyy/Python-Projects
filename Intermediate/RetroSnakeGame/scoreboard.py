f5d55b1270678605affd16bdc089ab68d5994894
Sighcyy <saisumedhar.a@gmail.com>
2026-06-11 12:57:31 -0500

    Update scoreboard.py

--- Intermediate/RetroSnakeGame/scoreboard.py
+++ Intermediate/RetroSnakeGame/scoreboard.py
@@ -6,9 +6,11 @@ FONT = ("Courier New", 24, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
+        self.current_score = 0
+        with open("data.txt", mode= "r") as file:
+            self.high_score = int(file.read())
         self.penup()
-        self.text = f"Score: {self.score}"
+        self.text = f"Score: {self.current_score}"
         self.color('white')
         self.hideturtle()
         self.speed('fastest')
@@ -16,15 +18,21 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.text = f"Score: {self.score}"
+        self.clear()
+        self.text = f"Score: {self.current_score} High Score: {self.high_score}"
         self.write(self.text, align= ALIGNMENT, font= FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", align= ALIGNMENT, font= FONT)
+    def reset_scoreboard(self):
+        if self.current_score > self.high_score:
+            self.high_score = self.current_score
+            with open(f"data.txt", mode= "w") as file:
+                file.write(str(self.current_score))
+        self.current_score = 0
+        self.update_score()
+
+
 
     def increase_score(self):
-        self.score += 1
-        self.clear()
+        self.current_score += 1
         self.update_score()
 
