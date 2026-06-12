14c558a70f253d6f7fa49e374ff76a4745617f66
Sighcyy <saisumedhar.a@gmail.com>
2026-06-12 18:25:42 -0500

    Add files via upload

---
+++ Intermediate/US_States_Quiz/main.py
@@ -0,0 +1,45 @@
+import turtle
+import pandas
+
+
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.bgcolor("white")
+image = "blank_states_img.gif"
+turtle.addshape(image)
+turtle.shape(image)
+
+guessed_states = []
+
+data = pandas.read_csv("50_states.csv")
+all_states = data["state"].to_list()
+missing_states = []
+
+
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="Guess an empty state (Say Exit to leave): ")
+    if answer_state != None:
+        answer_state = answer_state.title()
+
+    if answer_state == "Exit":
+        for states in all_states:
+            if states not in guessed_states:
+                missing_states.append(states)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("missing_states.csv")
+        break
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_coords = data[data["state"] == answer_state]
+        t.goto(state_coords["x"].item(), state_coords["y"].item())
+        t.write(answer_state)
+        guessed_states.append(answer_state)
+
+
+
+screen.exitonclick()
+
